4ed3ab9460aedf4c3ac958d437f782b1ca03605d
Bastian Hagedorn <bastianhagedorn@gmx.de>
2012-11-09 15:48:56 +0100

    QR-Zerlegung funktioniert, Lösungsfunktion muss noch geschrieben werden

--- blatt5.m
+++ blatt5.m
@@ -1,67 +1,71 @@
 function blatt5()
 A = [3 -6; 4 -8; 0 1];
-[QR, alpha] = qrDecomposition( A )
+[QR, alpha] = myQR( A );
+x = solveWithQR( QR, alpha, b )
 end
 
-function [ QR, a ] = qrDecomposition( A )
+function [ QR, alpha ] = myQR( A )
+    [m n QR R alpha] = initializeVariables(A);
+        
+    for i=1:n
+        % calculate and register v
+        columnAndRowErased = createSubmatrix(R, i);
+        v = calculateReflectionPlane( columnAndRowErased );
+        QR(i:m, i) = v;
+        
+        R = calculateR( R, v, i);
+        alpha(i) = R(i,i);       
+    end   
+    
+    QR = registerR( QR, R );
+end
+
+function [m n QR R alpha] = initializeVariables( A )
     [m n] = size(A);
     QR = zeros(m, n);
-    
-    [QR, Q] = fillQRWithReflectionPlanesAndCalculateQ(QR, A);
-    
-    R=Q*A;
-    QR = fillQRWithR(QR, R)
-    
-    a = 1;
-end
-
-function [QR, Q] = fillQRWithReflectionPlanesAndCalculateQ( QR, A )
-    [m n] = size(A);
-
-    % erster schritt auﬂerhalb der schleife
-    v = calculateReflectionPlane(A);
-    QR(1:m, 1) = v;
-    Q = calculateSubQ(v, 0);
-    
-    for i=2:n 
-        QA= Q*A
-        v = calculateReflectionPlane(createSubmatrix(QA, i));
-        QR(i:m, i) = v;
-       
-        % Q_i werden aufmultipliziert
-        Q = calculateSubQ(v, i-1) * Q;
-    end
-end
-
-function QR = fillQRWithR(QR, R)
-    
+    R = A;
+    alpha = zeros( min(m, n), 1);
     
 end
 
 function v = calculateReflectionPlane( M )
-    sgn = sign(M(1,1));
-    if (sgn == 0)
-        sgn = 1;
-    end
+    sigma = calculateSigma( M );
     euclidNorm = norm(M(:,1),2);
     v = M(:,1);
-    v(1) = v(1) + sgn * euclidNorm;
+    v(1) = v(1) + sigma * euclidNorm;
+end
+
+function sigma = calculateSigma( M )
+    sigma = sign(M(1,1));
+    if (sigma == 0)
+        sigma = 1;
+    end
 end
 
 function A = createSubmatrix( A, i )
-    [m n] = size(A);
-    A = A((i):m, (i):n);
+    if (i ~= 0)
+        [m n] = size(A);
+        A = A((i):m, (i):n);
+    end
 end
 
-function subQ = calculateSubQ( v, additionalUnitMatrixDimension )
-    [unitMatrixDimension n] = size(v);
-    unitMatrix = eye(unitMatrixDimension);
-    
-    tempQ = unitMatrix - (2/(v'*v))*(v*v');
-    
-    subQDimension = unitMatrixDimension + additionalUnitMatrixDimension;
-    subQ = eye(subQDimension);
-    
-    subQ((additionalUnitMatrixDimension+1):subQDimension, (additionalUnitMatrixDimension+1):subQDimension) = tempQ;
+function R = calculateR( R, v, insertPosition )
+    [m n] = size(R);
+
+    rightSizeV = zeros(m, 1);
+    rightSizeV( insertPosition:m, 1) = v;
+    R = R - (2/(rightSizeV'*rightSizeV)) * (rightSizeV*(rightSizeV'*R));
 end
+
+function QR = registerR( QR, R )
+    [m n] = size(R);
+    
+    for row = 1 : (min(m, n)-1)
+        for column = (row + 1) : min(m, n)
+            QR(row, column) = R(row, column);
+        end
+    end
+end
+
+
 
